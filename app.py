04c65892a6434fcb9743502234d68058c52d8ecb
Renat Haluska <haluska.renat@gmail.com>
2026-07-24 14:08:01 +0000

    Remove auto-refresh from competition page

--- app.py
+++ app.py
@@ -1031,7 +1031,6 @@ COMPETITION_HTML = (
     setInterval(tickCountdowns, 500);
     tickCountdowns();
   }
-  setInterval(() => location.reload(), 5000);
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
